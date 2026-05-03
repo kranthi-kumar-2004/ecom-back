6b9e4fad0e4616126a1e4c38fdb7f1bae55ce421
kranthi-kumar-2004 <159746543+kranthi-kumar-2004@users.noreply.github.com>
2026-05-03 20:26:32 +0530

    Update CorsConfig.java

--- src/main/java/com/student/management/security/CorsConfig.java
+++ src/main/java/com/student/management/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:5173") 
+                        .allowedOriginPatterns("https://ecomerce-front-fvu6.vercel.app") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization") 
